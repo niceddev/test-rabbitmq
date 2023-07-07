b2a58e29df28afd717cc502e4bdcf80150ba542f
Aslan <niceddeveloper@gmail.com>
2023-07-08 01:47:21 +0600

    1

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\TestJob;
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        TestJob::dispatch()->onQueue('test');
+        return 1;
+    }
+}
